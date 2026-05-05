648982261c015935e339743da62dcce6c32f1693
mindvoyage009-pixel <mindvoyage009@gmail.com>
2026-05-05 23:28:31 +0300

    Comment out base URL line in astro.config.mjs
    Commented out the base URL line for GitHub Pages usage.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://zakihub.eu.org',
+  // base: '/zakihub', // فعل هذا السطر لو استخدمت github.io/zakihub
   output: 'static',
   integrations: [sitemap()],
   build: {
